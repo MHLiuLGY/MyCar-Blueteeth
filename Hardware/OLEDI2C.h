02acfb51e164a4afdeab544750f8dd8f8185ec7e
MHLiuLGY <10222150413@stu.ecnu.edu.cn>
2023-12-03 19:44:42 +0800

    getstatus

--- Hardware/OLEDI2C.h
+++
@@ -1,9 +0,0 @@
-#ifndef __OLEDI2C_H__
-#define __OLEDI2C_H__
-
-void OLEDI2C_Init();
-void OLEDI2C_Start();
-void OLEDI2C_Stop();
-void OLEDI2C_SendByte(uint8_t Byte);
-
-#endif
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # MyCar
 
-此为去掉OLED的蓝牙控制+红外遥控控制版本。
+无OLED+蓝牙控制+红外遥控控制
+
+去掉OLED是因为增加蓝牙控制后引脚不够
 
 使用keil5，ARM Compiler version 6编译，下载至STM32F103C8T6最小系统板，运行即可
